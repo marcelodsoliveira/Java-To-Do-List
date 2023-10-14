a8807c80463e10bc7e71aa3623041ebb1b6018c0
Marcelo S Oliveira <oliveira.lnx@gmail.com>
2023-10-14 06:45:41 -0300

    Code is running but with a tip of Rocketseat discord user, that said to use startsWith instead equal in the contition to get authentication user on filtertask component.

--- src/main/java/br/com/marcelodsoliveira/todolist/task/filter/FilterTaskAuth.java
+++ src/main/java/br/com/marcelodsoliveira/todolist/task/filter/FilterTaskAuth.java
@@ -25,41 +25,37 @@ public class FilterTaskAuth extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
-        // Pegar a autenticação (user and pass)
-        var authorization = request.getHeader("Authorization");
-        
-        
-        var authEncoded = authorization.substring("Basic".length()).trim();
+    var servletPath = request.getServletPath();
 
-        byte[] authDecode =  Base64.getDecoder().decode(authEncoded);
+    if (servletPath.startsWith("/tasks")) {
+      // Pegar a autenticação (user and pass)
+      var authorization = request.getHeader("Authorization");
 
-        var authString = new String(authDecode);
-        
+      var authEncoded = authorization.substring("Basic".length()).trim();
 
-        String[] credentials = authString.split(":");
-        String username = credentials[0];
-        String password = credentials[1];
-     
+      byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
-        // Validar usuário
-          var user = this.userRepository.findByUsername(username);
-          if(user == null) {
-            response.sendError(401);
-          } else {
-            // Validar senha
-            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-            if(passwordVerify.verified) {
-              filterChain.doFilter(request, response);
-            } else {
-              response.sendError(401);
-            }
+      var authString = new String(authDecode);
 
-            
-          }
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
 
-
-   
+      // Validar usuário
+      var user = this.userRepository.findByUsername(username);
+      if (user == null) {
+        response.sendError(401);
+      } else {
+        // Validar senha
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+        if (passwordVerify.verified) {
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401);
+        }
+      }
+    } else {
+      filterChain.doFilter(request, response);
+    }
   }
-
-  
 }
